0906e437acc62f55ff47a66ce0513bac6c086b8b
Matthew Ecker <eckermd@gmail.com>
2017-03-16 12:01:49 -0400

    Abstracted stuff into new object Emu to get a gui working

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 mod gb;
+mod other;
 
 #[macro_use]
 extern crate sdl2;
@@ -13,35 +14,9 @@ fn main() {
     let     file_buf    = load_rom(file_name);
     gb::get_info::ret_info(&file_buf);
 
-    let mut cpu     = gb::cpu::Cpu::new();
-    let mut memory  = gb::memory::Memory::new();
-    memory.initialize();
-	
-    let mut ins     = file_buf[cpu.reg_pc as usize]; // keeps track of current instruction
+	let mut emu	= other::emu::Emu::new(file_buf);
 
-	let mut ins_count	= 0;
-
-    while cpu.cont {
-		cpu.print();
-
-        if ins == 0xCB {
-            ins = file_buf[(cpu.reg_pc + 1) as usize];
-            gb::instructions_cb::exec_ins_cb(&mut cpu, &mut memory, &file_buf, ins);
-        } else {
-            gb::instructions::exec_ins(&mut cpu, &mut memory, &file_buf, ins);
-        }
-
-		ins_count	+= 1;
-		if ins_count == 200 {
-			cpu.cont	= false;
-		}
-
-        ins = file_buf[cpu.reg_pc as usize];
-
-		if cpu.interrupt_count != 0 {
-			cpu.interrupt_handler();
-		}
-    }
+	other::display::render(&mut emu);
 }
 
 fn load_rom<P: AsRef<Path>>(path: P) -> Vec<u8> {
